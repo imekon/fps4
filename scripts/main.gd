286a48e4aab34b90a522fb0780223161802634db
Pete Goodwin <github@imekon.org>
2024-11-22 19:07:27 +0000

    fix tree issue

--- scripts/main.gd
+++ scripts/main.gd
@@ -11,8 +11,28 @@ func _ready():
 	var inst = floor.instantiate()
 	stage.add_child(inst)
 	
+	inst = floor.instantiate()
+	inst.position.z = -20
+	stage.add_child(inst)
+
+	inst = floor.instantiate()
+	inst.position.z = 20
+	stage.add_child(inst)
+
 	var woods = preload("res://scenes/woods_floor.tscn")
-	var inst2 = woods.instantiate()
-	inst2.position.x = -10
-	inst2.position.y = inst.position.y - 0.1
-	stage.add_child(inst2)
+	inst = woods.instantiate()
+	inst.position.x = -20
+	inst.position.y = inst.position.y - 0.1
+	stage.add_child(inst)
+	
+	inst = woods.instantiate()
+	inst.position.x = -20
+	inst.position.y = inst.position.y - 0.1
+	inst.position.z = -20
+	stage.add_child(inst)
+	
+	inst = woods.instantiate()
+	inst.position.x = -20
+	inst.position.y = inst.position.y - 0.1
+	inst.position.z = 20
+	stage.add_child(inst)
--- scripts/woods_floor.gd
+++ scripts/woods_floor.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
-func _onready():
+func _ready():
 	var tree = preload("res://scenes/tree.tscn")
 	
 	for y in 4:
 		for x in 4:
 			var inst = tree.instantiate()
 			inst.position.x = -8 + x * 4 + randi() % 3 - 1.5
+			inst.position.y = inst.position.y - 0.5
 			inst.position.z = -8 + y * 4 + randi() % 3 - 1.5
 			add_child(inst)
+
+	print('woods')
